6190384c8988e7bc0ce871d25ec097660105f708
Rajkumar <mandarajkumar2001>
2026-07-18 17:21:54 +0530

    nginx

--- catalogue.sh
+++ catalogue.sh
@@ -80,7 +80,7 @@ VALIDATE $? "Installing MongoDB Client"
 STATUS=$(mongosh --host mongodb.roboshop.cyou --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.roboshop.cyou </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
