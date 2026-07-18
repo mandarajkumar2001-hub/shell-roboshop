e588240a9e0e1fa6a062dc0a90b5e7da7f17ee68
Rajkumar <mandarajkumar2001>
2026-07-18 09:49:04 +0530

    remove Dispatch

--- roboshop .sh
+++ roboshop .sh
@@ -7,7 +7,8 @@ INSTANCES=("mongodb" "catalogue" "redis" "user" "payment"
 zone_ID="Z01235012H9P8159XBCQ5"
 DOMAIN_NAME="roboshop.cyou"
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-05b7ab0fa6a574779  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
