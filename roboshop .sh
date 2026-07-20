ab56f468220b751baba9f227520c9d53ded52d08
Rajkumar <mandarajkumar2001>
2026-07-20 11:23:04 +0530

    nginx

--- roboshop .sh
+++ roboshop .sh
@@ -10,19 +10,19 @@ DOMAIN_NAME="roboshop.cyou"
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-05b7ab0fa6a574779  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+   INSTANCE_ID=$(/usr/local/bin/aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-05b7ab0fa6a574779  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        IP=$(/usr/local/bin/aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(/usr/local/bin/aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
 
 
-aws route53 change-resource-record-sets \
+/usr/local/bin/aws route53 change-resource-record-sets \
     --hosted-zone-id $zone_ID \
     --change-batch '
     {
